2716d329841a2f21db4fa53ac34883e85a32313b
kai <kai@simbo.com.tw>
2020-08-31 23:43:56 +0800

    fix bug of paging next

--- src/components/paging.vue
+++ src/components/paging.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="btn-group" role="group" aria-label="Basic example">
     <button type="button" class="btn btn-primary" :disabled="hasPrevPage()" :class="{'disabled': hasPrevPage()}" v-on:click="changePage(pagination.current_page - 1)">上一頁</button>
-    <button type="button" class="btn btn-light">{{pagination.current_page}}</button>
+    <button type="button" class="btn btn-light" :disabled="true">{{pagination.current_page}}</button>
     <button type="button" class="btn btn-primary" :disabled="hasNextPage()" :class="{'disabled': hasNextPage()}" v-on:click="changePage(pagination.current_page + 1)">下一頁</button>
   </div>
 </template>
@@ -21,7 +21,7 @@ export default {
       return this.pagination.current_page === 1
     },
     hasNextPage () {
-      return this.pagination.current_page === this.pagination.total_pages
+      return this.pagination.current_page > this.pagination.total_pages || this.pagination.current_page === this.pagination.total_pages
     },
     changePage (expectPage) {
       this.$emit('change-page', expectPage)
